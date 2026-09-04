97ab14454fb4eb8435f7cd3fdfa20138be43c1a7
kaiotcp1 <kaiotcp1@gmail.com>
2026-09-04 18:47:24 -0300

    feat: add true for provision infraestructure

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,5 +1,5 @@
 locals {
   # Controlam somente os workflows; nunca condicionam recursos Terraform.
-  provision_infrastructure = false
+  provision_infrastructure = true
   destroy_infrastructure   = false
 }
