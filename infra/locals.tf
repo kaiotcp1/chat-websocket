ad854114a89f121e2753fd392bcc7a8aec699ef5
kaiotcp1 <kaiotcp1@gmail.com>
2026-09-04 20:49:47 -0300

    chore(infra): enable application teardown

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,5 +1,5 @@
 locals {
   # Controlam somente os workflows; nunca condicionam recursos Terraform.
-  provision_infrastructure = true
-  destroy_infrastructure   = false
+  provision_infrastructure = false
+  destroy_infrastructure   = true
 }
